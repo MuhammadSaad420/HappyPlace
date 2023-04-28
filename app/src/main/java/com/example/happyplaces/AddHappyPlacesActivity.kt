06f5f25cc368d9aa236a700157dc2b1f3e7c34b2
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-28 16:48:40 +0500

    using the third party library Dexter for permission handling

--- app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
@@ -1,13 +1,25 @@
 package com.example.happyplaces
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happyplaces.databinding.ActivityAddHappyPlacesBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddHappyPlacesActivity : AppCompatActivity(), View.OnClickListener {
     var binding: ActivityAddHappyPlacesBinding? = null
@@ -24,6 +36,7 @@ class AddHappyPlacesActivity : AppCompatActivity(), View.OnClickListener {
             onBackPressed()
         }
         binding?.etDate?.setOnClickListener(this)
+        binding?.tvAddImage?.setOnClickListener(this)
         dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
             cal.set(Calendar.YEAR,year)
             cal.set(Calendar.MONTH,month)
@@ -49,7 +62,56 @@ class AddHappyPlacesActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
             }
+            R.id.tv_add_image -> {
+                var myDialog = AlertDialog.Builder(this)
+                myDialog.setTitle("Pick Option")
+                val items = arrayOf("Gallery", "Camera")
+                myDialog.setItems(items) {_, which ->
+                    when(which) {
+                        0 -> choosePhotoFromGallery()
+                    }
+
+                }
+                myDialog.show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this)
+            .withPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddHappyPlacesActivity,"All permissions are granted", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest?>?,
+                    token: PermissionToken?
+                ) {
+                    showPermissionDeniedDialog()
+                }
+            }).check()
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this).setTitle("Permission Alert")
+            .setPositiveButton("Go to settings") {_,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel") {dialog,_ ->
+                dialog.dismiss()
+            }.show()
     }
 
 
