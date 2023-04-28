e57a23fc3c07a248a036b55fb1c5b316fdd76e9a
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-28 15:17:21 +0500

    Adding calender picker and populating the edit text field with it

--- app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
@@ -1,11 +1,18 @@
 package com.example.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.happyplaces.databinding.ActivityAddHappyPlacesBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlacesActivity : AppCompatActivity() {
+class AddHappyPlacesActivity : AppCompatActivity(), View.OnClickListener {
     var binding: ActivityAddHappyPlacesBinding? = null
+    var cal:Calendar = Calendar.getInstance();
+    lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlacesBinding.inflate(layoutInflater);
@@ -16,5 +23,34 @@ class AddHappyPlacesActivity : AppCompatActivity() {
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressed()
         }
+        binding?.etDate?.setOnClickListener(this)
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR,year)
+            cal.set(Calendar.MONTH,month)
+            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateTextView()
+        }
+
     }
+
+    private fun updateTextView() {
+        val myFormat = "MM/dd/yy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        binding?.etDate?.setText(dateFormat.format(cal.getTime()))
+    }
+
+    override fun onClick(v: View?) {
+        Log.e("pressed", "yes")
+        when(v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(this@AddHappyPlacesActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+
 }
